be766e5e28089bc20649e1d5b4dd4bd397a44ad1
JamesKessels <92016955+JamesKessels@users.noreply.github.com>
2024-01-15 12:37:54 +0100

    Puzzle Import Update

--- Assets/Importer.cs
+++ Assets/Importer.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using EasyButtons;
+using Unity.VisualScripting;
 
 public class Importer : MonoBehaviour
 {
     [SerializeField] private GameObject modelToImport;
+    [SerializeField] private List<List<GameObject>> ModelLayers = new List<List<GameObject>>();
+    [SerializeField] private GameObject importedModel;
+    [SerializeField] private GameObject currentLayer;
+    
+    [SerializeField] private GameObject puzzleContainer;
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,17 +26,68 @@ public class Importer : MonoBehaviour
         
     }
 
+    //Imports 3D Model
     [Button]
-    void Import()
+    private void Import()
     {
-        GameObject importedModel = Instantiate(modelToImport);
-        GameObject puzzleContainer = GameObject.FindWithTag("PuzzleContainer");
+        importedModel = Instantiate(modelToImport);
+        puzzleContainer = GameObject.FindWithTag("PuzzleContainer");
 
-        foreach (Transform child in importedModel.transform)
+        DeterminePieces(importedModel, puzzleContainer);
+    }
+
+    private void DeterminePieces(GameObject model, GameObject container)
+    {
+        ModelLayers.Clear();
+
+        if(model.transform.childCount == 1)
         {
-            child.transform.parent = puzzleContainer.transform;
+            foreach (Transform child in model.transform)
+            {
+                child.transform.parent = container.transform;
+            }
+
+            DestroyImmediate(model);
+
+            importedModel = container.transform.GetChild(0).gameObject;
+            DeterminePieces(importedModel, puzzleContainer);
         }
 
-        DestroyImmediate(importedModel);
+        else
+        {
+            foreach (Transform child in model.transform)
+            {
+                child.transform.parent = container.transform;
+            }
+
+            DestroyImmediate(model);
+
+            for (int childIndex = 0; childIndex < container.transform.childCount; childIndex++)
+            {
+                ModelLayers.Add(new List<GameObject>());
+                FindMeshes(container.transform.GetChild(childIndex).gameObject, childIndex);
+            }
+        }
     }
+
+    private void FindMeshes(GameObject layer, int pieceIndex)
+    {
+        currentLayer = GameObject.Find(layer.name).gameObject;
+
+        if (layer.transform.childCount > 0)
+        {
+            FindMeshes(layer.transform.GetChild(0).gameObject, pieceIndex);
+        }
+        else
+        {
+            Debug.Log(currentLayer);
+            ModelLayers[pieceIndex].Add(currentLayer);
+        }
+    }
+}
+
+[System.Serializable]
+public class ModelLayer
+{
+    public List<GameObject> subElements;
 }
